56a866699c1e4e507bf0d291902f8d3f30d013a5
Vinay Kumar Maheshwaram <vmaheshw@progress.com>
2026-02-26 12:18:04 +0530

    Add subtract method to Calculator class

--- src/main/java/com/demo/Calculator.java
+++ src/main/java/com/demo/Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
         return a + b;
     }
 
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
